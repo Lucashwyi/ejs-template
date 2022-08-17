2d81cd7a83ac814dce43b04dde67ae91a0fd7a40
Lucas Yi <103147047+Lucashwyi@users.noreply.github.com>
2022-08-17 02:47:34 +0000

    changes occurred to get full points

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   var title = 'Our Home Page';
-  var heading = 'My Website Name';//changes myfirstbootstap
+  var heading = 'The Fallout Franchise';//changes myfirstbootstap
    res.render('pages/index',{
      'title':title,
      'heading':heading
@@ -18,12 +18,50 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   var title = 'Our About Page';
-  var heading = 'My Website Name';//changes myfirstbootstap
+  var heading = 'The Fallout Franchise';
    res.render('pages/about',{
      'title':title,
      'heading':heading
    });//routes to pages
 })
+
+app.get('/aceu', (req, res) => {
+  var title = 'Aceu';
+  var heading = 'Aceu Content Creator';
+   res.render('pages/aceu',{
+     'title':title,
+     'heading':heading
+   });//routes to pages
+})
+
+app.get('/39daph', (req, res) => {
+  var title = '39daph';
+  var heading = '39daph Content Creator';
+   res.render('pages/39daph',{
+     'title':title,
+     'heading':heading
+   });//routes to pages
+})
+
+app.get('/trainwreck', (req, res) => {
+  var title = 'TrainwreckTV';
+  var heading = 'TrainwreckTV Content Creator';
+   res.render('pages/trainwreck',{
+     'title':title,
+     'heading':heading
+   });//routes to pages
+})
+
+app.get('/xqc', (req, res) => {
+  var title = 'XQC';
+  var heading = 'XQC Content Creator';
+   res.render('pages/xqc',{
+     'title':title,
+     'heading':heading
+   });//routes to pages
+})
+
+
 
 
 app.listen(port, () => {
